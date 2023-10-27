b22e50786d140ec55f47806eb369aa0a19a2c44b
JonathanJM1 <117751079+JonathanJM1@users.noreply.github.com>
2023-10-27 16:32:12 +0200

    index.jsx

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,14 +1,18 @@
+// Importer la bibliothèque React
 import React from "react";
 
-function Banner(props) {
-  // En fonction de la valeur de props.h1, il affiche ou non un titre
+function Banner (props) {
+
   return (
     <section className="banner">
       <div className={"banner__" + props.div + "img"}>
-        {props.h1 && <h1 className="banner__title">{props.title}</h1>}
-      </div>
-    </section>
-  );
-}
+      {props.h1 ? (
+                    <h1 className="banner__title">{props.title}</h1>
+                  ) : 
+                ("")}
+            </div>
+        </section>
+    );
+};
 
 export default Banner;
